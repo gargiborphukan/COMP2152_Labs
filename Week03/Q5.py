bf3e37e782607d5eb8636e38a95d60eba16adebf
gargi (gargiborphukan) <gargiborphukan87@gmail.com>
2026-02-09 16:25:58 -0500

    Add 04

---
+++ Week03/Q5.py
@@ -0,0 +1,9 @@
+#Question 5: Contact Book (Dictionaries - Basic Operations)
+contacts = {
+    "Alice" : "555-1234",
+    "Bob" : "555-5678",
+    "Charlie" : "555-9999"
+}
+print(f"Alice's number : {contacts["Alice"]}")
+contacts["Diana"] = "555-4321"
+print(f"Contacts after adding Diana: {contacts}")
